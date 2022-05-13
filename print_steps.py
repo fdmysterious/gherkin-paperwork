a02dd035be2bd1ab8ce0ea43a2bf56ef1f84df11
Florian Dupeyron <florian.dupeyron@elsys-design.com>
2022-05-14 00:00:09 +0200

    Parse EN and FR feature files and pprint for easier reading

--- print_steps.py
+++ print_steps.py
@@ -1,13 +1,18 @@
 from gherkin.parser                 import Parser
 from gherkin_paperwork.feature_file import Scenario, Step
 
+from pprint                         import pprint
 
-if __name__ == "__main__":
+def parse_featf(fpath):
     parser = Parser()
 
-    feature_file = parser.parse("test.feature")
+    feature_file = parser.parse(fpath)
     scenario_0   = feature_file["feature"]["children"][0]["scenario"]
 
     sc_obj       = Scenario.from_dict(scenario_0)
 
-    print(sc_obj)
+    pprint(sc_obj)
+
+if __name__ == "__main__":
+    parse_featf("test.feature")
+    parse_featf("test_fr.feature")
