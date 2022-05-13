41b5fffc7070d86f921743d13428068b528eae47
Florian Dupeyron <florian.dupeyron@elsys-design.com>
2022-05-13 19:16:36 +0200

    Pretty print output dict. for better understanding, remove useless imports

--- print_ast.py
+++ print_ast.py
@@ -1,20 +1,13 @@
-from gherkin.token_scanner import TokenScanner
-from gherkin.token_matcher import TokenMatcher
-
 from gherkin.parser        import Parser
-from gherkin.errors        import ParserError
-
-from pathlib               import Path
+from pprint                import pprint
 
 
 def print_parsed_file(fpath):
         parser        = Parser()
         feature_file  = parser.parse(str(fpath))
 
-        print(feature_file)
-        print(type(feature_file))
+        pprint(feature_file)
 
 if __name__ == "__main__":
     print_parsed_file("test.feature")
     print_parsed_file("test_fr.feature")
-
