0e1c4087146c8435ac54024239618b40df8f898e
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-03-09 21:12:26 +0100

    Move tests into tests folder

---
+++ src/gherkin_paperwork/__init__.py
@@ -0,0 +1,8 @@
+"""
+==================================================================
+Simple wrapper around the gherkin library for documentation stuff.
+==================================================================
+
+:Authors: - Florian Dupeyron <florian.dupeyron@mugcat.fr>
+:Date: March 2023
+"""
